fec5a772ca5e104170703d3f526e4101a259ce2b
Hamza-Sahapurwala <hamjuzyt@gmail.com>
2025-05-05 06:25:52 +0530

    User Login

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
+#include<conio.h>
+#include<string.h>
 
 void signup();
 void login();
+int checkCredentials(const char *username, const char *password);
 
 int main(){
-
-    while(1){
     
     printf("Welcome!!!\n");
+    
+    while(1){
+
     printf("What would like to do?\n");
     printf("1.Login\n");
     printf("2.Sign-Up\n");
@@ -22,7 +26,7 @@ int main(){
 
         case 1:
 
-        printf("1\n");
+        login();
 
         break;
 
@@ -49,16 +53,53 @@ int main(){
 }
 
 void signup(){
+
     char username[100],password[100];
-    printf("Enter Username:\n");
-    scanf("%[^\n]s",username);
+    printf("Enter Username:(No Spaces)");
+    scanf("%s",username);
     getchar();
 
-    printf("Enter password:\n");
-    scanf("%[^\n]s",password);
+    printf("Enter password:(No Spaces)");
+    scanf("%s",password);
     getchar();
 
     printf("%s\n",username);
     printf("%s\n",password);
 
+    FILE* sign = fopen("users.csv","a+");
+
+    fprintf(sign,"%s | %s\n", username, password);
+
+    fclose(sign);
+
+}
+
+void login(){
+
+    char username[100],password[100];
+    printf("Enter Username:(no spaces)");
+    scanf("%s",username);
+    getchar();
+
+    printf("Enter password:(no spaces)");
+    scanf("%s",password);
+    getchar();
+
+    FILE* login = fopen("users.csv","r");
+
+    // * doesn't do shit from here onwards
+
+    char line[1024];
+    while (fgets(line, 1024, login)) {
+        line[strcspn(line, "\n")] = 0;
+        char *token = strtok(line, ",");
+        while (token != NULL) {
+            printf("%s ", token);
+            token = strtok(NULL, ",");
+        }
+        printf("\n");
+    }
+
+    fclose(login);
+    
 }
